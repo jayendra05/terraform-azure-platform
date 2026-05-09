8da912175b1d4b2be369f70772221f1464b73dc9
Jayendra <jayendra06mishra@gmail.com>
2026-05-09 21:59:40 +0530

    Updated Terraform backend storage account

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,26 +1,8 @@
 terraform {
-  required_version = ">=1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-module "resource_group" {
-  source = "../../modules/resource-group"
-
-  resource_group_name = var.resource_group_name
-  location            = var.location
-
-  tags = {
-    environment = "dev"
-    project     = "terraform-platform"
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstateplatform002"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
   }
 }
