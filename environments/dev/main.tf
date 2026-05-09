123a663bfddb9b15d539162b8630275d90247e75
Jayendra <jayendra06mishra@gmail.com>
2026-05-09 21:18:33 +0530

    first commit

---
+++ environments/dev/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">=1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source = "../../modules/resource-group"
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  tags = {
+    environment = "dev"
+    project     = "terraform-platform"
+  }
+}
