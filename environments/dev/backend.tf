2a0c21991f61f4aa19d19d04ec7d2cd9718ce1e9
Jayendra <jayendra06mishra@gmail.com>
2026-05-09 23:15:30 +0530

    Fixed Azure DevOps YAML formatting

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateplatform001"
+    storage_account_name = "tfstateplatform002"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
