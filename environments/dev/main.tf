3f50f38a980e689f64b543056b68fcb505384d50
Jayendra <jayendra06mishra@gmail.com>
2026-05-10 01:16:17 +0530

    Adding resource group

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -24,3 +24,15 @@ module "resource_group" {
     project     = "terraform-platform"
   }
 }
+
+module "resource_group_02" {
+  source = "../../modules/resource-group"
+
+  resource_group_name = "rg-dev-network-001"
+  location            = "Central India"
+
+  tags = {
+    environment = "dev"
+    project     = "terraform-network"
+  }
+}
