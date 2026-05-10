865b3eb413c4064309fb976502ae4bbd20e4e16f
Jayendra <jayendra06mishra@gmail.com>
2026-05-10 17:35:02 +0530

    Revert "Adding resource group"
    This reverts commit 3f50f38a980e689f64b543056b68fcb505384d50.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,11 +7,14 @@ module "resource_group_platform" {
   tags = local.common_tags
 }
 
-module "resource_group_network" {
+module "resource_group_02" {
   source = "../../modules/resource-group"
 
-  resource_group_name = var.network_resource_group_name
-  location            = var.location
+  resource_group_name = "rg-dev-network-001"
+  location            = "Central India"
 
-  tags = local.common_tags
+  tags = {
+    environment = "dev"
+    project     = "terraform-network"
+  }
 }
